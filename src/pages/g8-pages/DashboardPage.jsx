d740dd0dddaa3a276eac7ace17e901513d9dd7b8
GoBrazill <vinicius.daraujos26@gmail.com>
2026-04-27 11:09:42 +0000

    chore: correct dashboard card import and add props for test card

--- src/pages/g8-pages/DashboardPage.jsx
+++ src/pages/g8-pages/DashboardPage.jsx
@@ -1,11 +1,11 @@
-import DashBoardCards from "../../components/DashboardCards";
+import DashBoardCard from "../../components/DashboardCard";
 import { DashboardSectionStyle } from "./DashboardSectionStyle";
 
 
 export default function Dashboard(){
     return (
         <DashboardSectionStyle >
-            <DashBoardCards />
+            <DashBoardCard colSpan={3} rowSpan={2} title={'Esse é um título'} description={'Essa é a descrição'}/>
         </DashboardSectionStyle>
     )
 }
