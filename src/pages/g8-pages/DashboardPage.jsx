31b21c2bfc53df0ab8c9e07d8274bac51599938b
GoBrazill <vinicius.daraujos26@gmail.com>
2026-05-04 13:34:16 -0300

    test: add some more cards to test responsiveness

--- src/pages/g8-pages/DashboardPage.jsx
+++ src/pages/g8-pages/DashboardPage.jsx
@@ -5,7 +5,13 @@ import { DashboardSectionStyle } from "./DashboardSectionStyle";
 export default function Dashboard(){
     return (
         <DashboardSectionStyle >
-            <DashBoardCard colSpan={3} rowSpan={2} title={'Esse é um título'} description={'Essa é a descrição'}/>
+            <DashBoardCard colSpan={3} rowSpan={4} title={'Esse é um título'} description={'Essa é a descrição'}/>
+            <DashBoardCard colSpan={1} rowSpan={2} title={'Esse é um título'} description={'Essa é a descrição'}/>
+            <DashBoardCard colSpan={1} rowSpan={2} title={'Esse é um título'} description={'Essa é a descrição'}/>
+            <DashBoardCard colSpan={1} rowSpan={1} title={'Esse é um título'} description={'Essa é a descrição'}/>
+            <DashBoardCard colSpan={1} rowSpan={1} title={'Esse é um título'} description={'Essa é a descrição'}/>
+            <DashBoardCard colSpan={1} rowSpan={1} title={'Esse é um título'} description={'Essa é a descrição'}/>
+            <DashBoardCard colSpan={1} rowSpan={1} title={'Esse é um título'} description={'Essa é a descrição'}/>
         </DashboardSectionStyle>
     )
 }
