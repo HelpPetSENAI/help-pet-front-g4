8f4dae285b5396444080f3b8a60e3083d529db52
GoBrazill <vinicius.daraujos26@gmail.com>
2026-05-16 09:04:47 -0300

    refactor: replace placeholder title and descriptionn

--- src/pages/g8-pages/DashboardPage.jsx
+++ src/pages/g8-pages/DashboardPage.jsx
@@ -6,7 +6,7 @@ import { DashboardSectionStyle } from "./DashboardSectionStyle";
 export default function Dashboard(){
     return (
         <DashboardSectionStyle >
-            <DashBoardCard colSpan={3} rowSpan={4} title={'Esse é um título'} description={'Essa é a descrição'} content={<BarChart />}/>
+            <DashBoardCard colSpan={3} rowSpan={4} title={'Gráfico de barra'} description={'Quantidades de  requisições e respostas divididas por dias da semana'} content={<BarChart />}/>
             <DashBoardCard colSpan={1} rowSpan={2} title={'Esse é um título'} description={'Essa é a descrição'}/>
             <DashBoardCard colSpan={1} rowSpan={2} title={'Esse é um título'} description={'Essa é a descrição'}/>
         </DashboardSectionStyle>
