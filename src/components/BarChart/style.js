1d1598a09f72a0801c79996afeac767fb15e9646
GoBrazill <vinicius.daraujos26@gmail.com>
2026-05-25 10:57:30 -0300

    chore: make small change to barChart

--- src/components/BarChart/style.js
+++ src/components/BarChart/style.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledCanvasWrapper = styled.div`
   position: relative;
   width: 100%;
+  height: auto;
   min-height: 350px;
   max-height: 650px;
   padding: 1rem 0;
